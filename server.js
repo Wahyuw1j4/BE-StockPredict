8c66185e5709cbb8e0a1c3cb944d97c26b196a2e
Wahyu Wijaya <wahyuw1j4@gmail.com>
2023-09-03 21:48:25 +0700

    add cors ip

--- server.js
+++ server.js
@@ -37,6 +37,13 @@ app.use(cors({
   }
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // You can specify specific origins instead of '*'
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
